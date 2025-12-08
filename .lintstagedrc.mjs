f09d8f0f3944b302f9bac5fa89a3d102b481295d
Grace Kim Howard <gracekimhoward@eaglerockanalytics.com>
2025-12-08 15:28:00 -0800

    chore: set up git hooks with husky, lint-staged, and commitlint

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,14 @@
+import path from "path";
+
+const buildEslintCommand = (filenames) =>
+  `next lint --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+export default {
+  "**/*.ts?(x)": [
+    "prettier --write",
+    buildEslintCommand,
+  ],
+  "**/*.{js,jsx}": ["prettier --write", buildEslintCommand],
+};
