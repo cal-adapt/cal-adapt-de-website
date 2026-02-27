e8c82d94bd1b93337764c728584e906a8a1cdb82
Grace Kim Howard <gracekimhoward@eaglerockanalytics.com>
2026-02-27 14:58:09 -0800

    chore: upgrade to node v24 and set up vitest, storybook with ci (#24)

---
+++ vitest.config.ts
@@ -0,0 +1,60 @@
+/// <reference types="vitest" />
+
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+const dirname =
+  typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  base: "./",
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/testing/setup-tests.ts",
+    exclude: ["**/node_modules/**", "**/e2e/**"],
+    coverage: {
+      include: ["src/**"],
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
